76a9a7d725f9775543bd9a52c272b1a56b0309c3
kokdot <kok.74@mail.ru>
2020-03-17 11:22:29 +0000

    Lesson3+5

--- station.rb
+++ station.rb
@@ -6,32 +6,33 @@
 # Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, 
 # находящихся на станции).
 class Station
-	attr_reader :name
+  attr_reader :name
 
-	def initialize(name)
-		@name = name
-		@trains = []
-	end
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
 
-	def to_s
-		@name
-	end
+  def to_s
+    @name
+  end
 
-	def get_train(train)
-	  @trains << train
-	end
+  def get_train(train)
+    @trains << train
+  end
 
-	def send_train(train)
-		@trains.delete(train)
-	end
+  def send_train(train)
+    @trains.delete(train)
+  end
 
-	def list_trains
-		@trains.each { |train| puts train.number }
-	end
+  def list_trains
+    @trains.each { |train| puts train.number }
+  end
   
-	def type_trains
+  def type_trains
     cargo = @trains.count { |train| train.type == 'грузовой' }
     passenger = @trains.count { |train| train.type == 'пассажирский' }
-		puts "В данный момент на станци находится #{ cargo } грузовых поездов и #{ passenger } пассажирских поездов."
-	end
+    puts "В данный момент на станци находится #{ cargo } грузовых поездов и #{ passenger } пассажирских поездов."
+  end
 end
+       
--- train.rb
+++ train.rb
@@ -13,15 +13,15 @@
 # только на 1 станцию за раз.
 # Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
 class Train
-	attr_writer :route
-	attr_reader :type, :quantity_vans
-	def initialize(number, type = true, vans)
-		@number = number
-		@type = type
-		@vans = vans
-		@speed = 0
-		@station_index = 0
-	end
+  attr_writer :route
+  attr_reader :type, :quantity_vans
+  def initialize(number, type = true, vans)
+    @number = number
+    @type = type
+    @vans = vans
+    @speed = 0
+    @station_index = 0
+  end
 
   def speed(delta)
     if @speed + delta < 0 
@@ -42,16 +42,16 @@ class Train
   def station_next
     @route.stations[@station_index + 1]
   end  
-	
-	def move_ahead
-  	if @route == nil
-			puts "Вы не можете начать движение, потому что у Вас нет маршрута"
-		else
-			if @station_index == @route.stations.size - 1
-				puts "Вы не можете двигаться вперед, потому что это последняя станция вашего маршрута"
-			else
-				@station_index += 1
-			end
+  
+  def move_ahead
+    if @route == nil
+      puts "Вы не можете начать движение, потому что у Вас нет маршрута"
+    else
+      if @station_index == @route.stations.size - 1
+        puts "Вы не можете двигаться вперед, потому что это последняя станция вашего маршрута"
+      else
+        @station_index += 1
+      end
     end
   end
 
@@ -59,35 +59,36 @@ class Train
     if @route == nil
       puts "Вы не можете начать движение, потому что у Вас нет маршрута"
     else
-		  if @station_index == 0
-			  puts "Вы не можете двигаться назад, потому что это первая станция вашего маршрута"
-		  else
-			   @station_index -= 1
-		  end
-	  end
-	end
+      if @station_index == 0
+        puts "Вы не можете двигаться назад, потому что это первая станция вашего маршрута"
+      else
+         @station_index -= 1
+      end
+    end
+  end
 
-	def van_add
-		if speed != 0
-			puts "Операции с вагонами можно производить только во время остановки поезда!"
-		else
-			@vans += 1
+  def van_add
+    if speed != 0
+      puts "Операции с вагонами можно производить только во время остановки поезда!"
+    else
+      @vans += 1
     end
   end
 
   def van_remove
-		if speed != 0
+    if speed != 0
       puts "Операции с вагонами можно производить только во время остановки поезда!"
     else
-			if @vans == 0
-				puts "Вы не можете отцеплять вагоны, потому что у Вас их больше нет"
-			else
-					@vans -= 1
-			end
-		end
-	end
+      if @vans == 0
+        puts "Вы не можете отцеплять вагоны, потому что у Вас их больше нет"
+      else
+          @vans -= 1
+      end
+    end
+  end
   
-	def vans
-		@vans
-	end
+  def vans
+    @vans
+  end
 end
+      
